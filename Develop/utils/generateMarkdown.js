4ee460874cc21419bc52e15da43a7364b1e59bc7
nita9801 <mrschristinemlopez@gmail.com>
2025-02-01 23:07:04 -0500

    generate markdown code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,44 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
+// a function that returns a license badge based on which license is passed in
 export default generateMarkdown;
+function renderLicenseBadge(license) {
+    if (license === 'MIT') {
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';''
+    } else if (license === 'Apache 2.0') {
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    } else if(license === 'GPL 3.0') {
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'; 
+    } else{
+      return ''; 
+      // If there is no license, return an empty string
+    }
+      // Function to generate markdown for README
+   
+function generateMarkdown(data) {
+    return` 
+      # ${data.title} 
+
+        ## Table of Contents
+         1. [Description](#description)
+         2. [Installation](#installation)
+         3. [Usage](#usage)
+         4. [License](#license)
+         5. [Credits](#credits)
+         6. [Tests](#tests)
+         7. [Questions](#questions) 
+
+        ## Description
+          ${data.Description}
+        ## Installation
+          ${data.installation}
+        ## Usage
+          ${data.Usage}
+        ## License
+          ${data.License}
+        ## Credits
+          ${data.Credits}      
+        ## Tests
+          ${data.Tests}
+        ## Questions
+           For any questions, please reach out to me at ${data.email} or visit my GitHub profile: ${data.Github}`
+     };
+ }
